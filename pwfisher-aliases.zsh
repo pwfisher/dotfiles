49242fdd81f86bf6368d39c5057a5a02a71e0460
Patrick Fisher <patrick@pwfisher.com>
2021-04-06 12:47:21 -0700

    alias gpom, gs, gitlog

--- pwfisher-aliases.zsh
+++ pwfisher-aliases.zsh
@@ -1,4 +1,7 @@
-alias ll='ls -alh'
-alias gti='git'
-
 alias kill_esets='pgrep esets | xargs sudo kill -9'
+alias ll='ls -alh'
+# plus! https://github.com/ohmyzsh/ohmyzsh/blob/master/plugins/git/git.plugin.zsh#L46
+alias gpom='git pull origin master'
+alias gs='git status'
+alias gti='git'
+alias gitlog='git log -"$(git rev-list --count HEAD ^origin/master)"'
--- pwfisher-terminal-titles.zsh
+++ pwfisher-terminal-titles.zsh
@@ -3,7 +3,7 @@ setTerminalText () { local mode=$1 ; shift ; echo -ne "\033]$mode;$@\007" }
 stt_both  () { setTerminalText 0 $@; }
 stt_tab   () { setTerminalText 1 $@; }
 stt_title () { setTerminalText 2 $@; }
-# me:
+# pwfisher:
 DISABLE_AUTO_TITLE="true"
 stt_tab "${PWD##*/} · .zprofile"
 function precmd () { stt_tab "${PWD##*/}" }
